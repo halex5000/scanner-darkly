d79db0e682cb5d3c49e9629817788be563f42476
Alexander Hardman <alexander.hardman@gmail.com>
2022-11-03 13:40:59 -0400

    trying to optimize the page load and response time

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,28 @@
-/* eslint-disable @typescript-eslint/naming-convention */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/naming-convention */
+
 import React from 'react';
 import ReactDOM from 'react-dom/client.js';
-import {withLDProvider} from 'launchdarkly-react-client-sdk';
+import {withLDProvider, asyncWithLDProvider} from 'launchdarkly-react-client-sdk';
 import {render} from 'react-dom';
 import App from './App';
 
-const LaunchDarklyProvider = withLDProvider({
-	clientSideID: import.meta.env.VITE_CLIENT_SIDE_ID,
-})(App);
+// Const LaunchDarklyProvider = withLDProvider({
+// 	clientSideID: import.meta.env.VITE_CLIENT_SIDE_ID,
+// })(App);
 
-// eslint-disable-next-line unicorn/prefer-query-selector
-const rootElement = document.getElementById('root');
-render(<LaunchDarklyProvider/>, rootElement);
+// // eslint-disable-next-line unicorn/prefer-query-selector
+// const rootElement = document.getElementById('root');
+// render(<LaunchDarklyProvider/>, rootElement);
+
+(async () => {
+	const LDProvider = await asyncWithLDProvider({
+		clientSideID: import.meta.env.VITE_CLIENT_SIDE_ID,
+	});
+	render(
+		<LDProvider>
+			<App/>
+		</LDProvider>,
+		document.querySelector('#root'),
+	);
+})();
