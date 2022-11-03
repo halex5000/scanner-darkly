0a70a843454a643624c62c5d28cf1457958a7901
Alexander Hardman <alexander.hardman@gmail.com>
2022-11-03 13:51:42 -0400

    tweaking bundle and registration

--- src/main.tsx
+++ src/main.tsx
@@ -1,28 +1,15 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/naming-convention */
-
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import React from 'react';
 import ReactDOM from 'react-dom/client.js';
-import {withLDProvider, asyncWithLDProvider} from 'launchdarkly-react-client-sdk';
+import {withLDProvider} from 'launchdarkly-react-client-sdk';
 import {render} from 'react-dom';
 import App from './App';
 
-// Const LaunchDarklyProvider = withLDProvider({
-// 	clientSideID: import.meta.env.VITE_CLIENT_SIDE_ID,
-// })(App);
+const LaunchDarklyProvider = withLDProvider({
+	clientSideID: import.meta.env.VITE_CLIENT_SIDE_ID,
+})(App);
 
-// // eslint-disable-next-line unicorn/prefer-query-selector
-// const rootElement = document.getElementById('root');
-// render(<LaunchDarklyProvider/>, rootElement);
-
-(async () => {
-	const LDProvider = await asyncWithLDProvider({
-		clientSideID: import.meta.env.VITE_CLIENT_SIDE_ID,
-	});
-	render(
-		<LDProvider>
-			<App/>
-		</LDProvider>,
-		document.querySelector('#root'),
-	);
-})();
+// eslint-disable-next-line unicorn/prefer-query-selector
+const rootElement = document.getElementById('root');
+render(<LaunchDarklyProvider/>, rootElement);
