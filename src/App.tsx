c5b4541ce43e61f458e85ef109b57d120b64e7ee
Alexander Hardman <alexander.hardman@gmail.com>
2022-11-03 12:25:43 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable unicorn/filename-case */
+import './App.css';
+
+function App() {
+	return (
+		<div className='App'>
+			<h1>ScannerDarkly</h1>
+		</div>
+	);
+}
+
+export default App;
